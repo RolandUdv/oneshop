2f532daa227a205c6e158abe5babaae4c7c542fc
rolandudv <u.roland58@gmail.com>
2020-02-24 23:35:02 +0000

    refined scan

--- resources/views/pages/scan.blade.php
+++ resources/views/pages/scan.blade.php
@@ -23,32 +23,49 @@
     </div>
 </div> --}}
 
+{{-- https://webqr.com/ --}}
+{{-- https://codepen.io/SebastianFlorica/pen/NbdYbj --}}
+
+<style>
+#v {
+    width: 100%!important;
+    border-radius: 30px;
+    box-shadow: 3px 3px 10px grey;
+    /* outline: 3px solid #000000; */
+}
+</style>
+
 <script type="text/javascript" src="https://webqr.com/llqrcode.js"></script>
 <script type="text/javascript" src="https://webqr.com/webqr.js"></script>
-
-<div id="main">
-    <div id="header">
-        <div style="position:relative;top:+20px;left:0px;">
+<div class="container">
+    <div id="main">
+        <div id="header">
             <g:plusone size="medium"></g:plusone>
         </div>
+    </div>
+</div>
 
-
-    <div id="mainbody">
-                            <img class="selector" id="webcamimg" src="https://webqr.com/vid.png" onclick="setwebcam()" />
-                            <img class="selector" id="qrimg" src="https://webqr.com/cam.png" onclick="setimg()" style="display:none;" />
-                        </br>
-                            <div class="container text-center">
-                            <td colspan="2" align="center">
-                                <div id="outdiv"></div>
+<div class="container">
+        <div id="mainbody" class="float-center">
+            <img class="selector" id="webcamimg" src="https://webqr.com/vid.png" onclick="setwebcam()"  style="display:none;" />
+            <img class="selector" id="qrimg" src="https://webqr.com/cam.png" onclick="setimg()" style="display:none;" />
+            </br>
+        </div>
+            <div class="container text-center">
+                <td colspan="2" align="center">
+                    <div id="outdiv"></div>
+                    </br>
 
                     <div id="result"></div>
-    
-<canvas id="qr-canvas" width="800" height="600" style="display: none;"></canvas>
 
+                    <canvas id="qr-canvas" width="800" height="600" style="display: none;"></canvas>
+                    
+                    </br>
+                    </br>
+                    <small>Credits Lazar Laszlo</small>
+            </div>
+        </div>
 </div>
-</div>
-
-
 
 <script type="text/javascript">
     load();
