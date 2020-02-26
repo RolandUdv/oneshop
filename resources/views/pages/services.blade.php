46b8bacc0886c9c706b39b003c82d08ca578f290
rolandudv <u.roland58@gmail.com>
2020-02-26 16:39:36 +0000

    Services design changes

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -5,17 +5,99 @@
     <h1>{{$title}}</h1></br>
 </div>
     {{-- @for ($i = 0; $i<100; $i++) --}}
-    <div class="container">
-        <div id="accordion">
-            @foreach($services as $service)
-            <div class="card">
-                <div class="card-header" id="headingOne"">
-                    <h5 class="mb-0">
-                        <button class="btn btn-link" data-toggle="collapse" data-target="#collapse{{$service->service_id}}" aria-expanded="true" aria-controls="collapse{{$service->service_id}}">
-                                {{$service->service_name}}
-                                {{-- <h3><span class="badge badge-secondary float-right">New</span></h3> --}}
+<style>
+.service-price-txt, .service-name-txt {
+    font-weight: 600;
+}
+
+.btn-link {
+    color: #000000!important;
+    font-weight: 600;
+}
+
+.col-sm-8 {
+    margin-bottom: 20px;
+}
+
+th {
+    float: left;
+    font-weight: 800!important;
+    font-size: 18px;
+}
+
+td {
+    float: right;
+    font-size: 18px;
+}
+
+.col-sm-4 {
+    background-color: #eaeaea;
+    /* box-shadow: 2px 2px 5px; */
+    border-radius: 20px;
+    padding: 10px;
+}
+
+.telno-widget, .opentimes-widget {
+    padding: 10px;
+    border-radius: 20px;
+    min-height: 100px;
+    height: auto;
+    /* background-color: blue; */
+    /* background-color: #F7F7F7; */
+    width: 100%;
+}
+
+.btn-primary {
+    background-color: #FF5864!important;
+}
+
+.callbtn {
+    border-radius: 20px;
+    width: 100%;
+    background-color: #FF5864;
+}
+
+.callbtn:hover{
+    /* background-color: ; */
+}
+
+.float-left {
+    margin-right: 10px;
+}
+
+.font-italic {
+    margin-top: 10px;
+}
+
+.progress {
+    background-color: #525252;
+    margin-bottom: -10px;
+    height: 25px;
+    border-radius: 20px;
+    max-width: 100%;
+}
+.progress-bar {
+    background: rgb(255,95,109);
+    background: linear-gradient(75deg, rgb(255, 86, 100) 0%, rgb(255, 191, 101) 100%);
+}
+
+</style>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-8">
+            <div id="accordion">
+                @foreach($services as $service)
+                <div class="card">
+                    <div class="card-header" id="headingOne"">
+                    <h5 class=" mb-0">
+                        <button class="btn btn-link" data-toggle="collapse"
+                            data-target="#collapse{{$service->service_id}}" aria-expanded="true"
+                            aria-controls="collapse{{$service->service_id}}">
+                            <h4 class="service-name-txt">{{$service->service_name}}</h4>
+                            {{-- <h3><span class="badge badge-secondary float-right">New</span></h3> --}}
                             {{-- <a class="btn btn-primary float-right" data-toggle="modal" data-target="#ModalView1" href="#hello">BOOK</a> --}}
-                        <a href="services/{{$service->service_id}}/create">
+                            <a href="services/{{$service->service_id}}/create">
                                 {{-- <button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target="#ModalView1">
                                     BOOK
                                 </button> --}}
@@ -23,12 +105,13 @@
                                     BOOK
                                 </button>
                             </a>
-                            <button class="btn btn-link float-right">£ {{$service->service_price}}</button>
-                            <button class="btn btn-link float-right"><small>(@include('inc.service_length'))</small></button>
+                            <button class="btn btn-link float-right"><h4 class="service-price-txt">£ {{$service->service_price}}</h4></button>
+                            <button
+                                class="btn btn-link float-right"><small>(@include('inc.service_length'))</small></button>
                             {{-- <div class="float-right">{{$booking->service_price}}</div> --}}
-                        </button>
+                    </button>
                     </h5>
-                    
+
                 </div>
                 <div id="collapse{{$service->service_id}}" class="collapse" aria-labelledby="headingOne"
                     data-parent="#accordion">
@@ -40,6 +123,7 @@
             @endforeach
         </div>
 
+        <!-- modal -->
         {{-- Pop up information --}}
         {{-- @if() --}}
         {{-- @foreach($bookings as $booking) --}}
@@ -74,11 +158,89 @@
                 </div>
               </div>
               {{-- @endforeach --}}
+    {{-- </div> <!-- end of col-sm-8 --> --}}
+    <div class="col-sm-4">
+        <div class="googlemaps-widget">
+            <h3 class="text-center font-weight-bold">Find Us</h3>
+            {{-- https://www.embedgooglemap.net/ --}}
+        {{-- <iframe width="100%" height="auto" frameborder="0" style="border:0"
+src="https://www.google.com/maps/embed/v1/undefined?origin=...&q=...&destination=...&center=...&zoom=...&key=..." allowfullscreen></iframe>
+        </div> --}}
 
+        {{-- <div class="mapouter"> --}}
+            <div class="gmap_canvas"><iframe width="100%" frameborder="0" height="250px" style="border-radius: 20px;" 
+                    src="https://maps.google.com/maps?q=city%20barbers&t=&z=15&ie=UTF8&iwloc=&output=embed"
+                    frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                </div>
 
-              
+        {{-- </div> --}}
 
+    </br>
 
+        <div class="rating-widget">
+        <h1 class="text-center font-weight-bold">Rating 4.5/5</h1>
+        {{-- <h3 class="text-white">1</h3> --}}
+        <h4 class="float-left">5 </h4>
+        <div class="progress">
+            <div class="progress-bar" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0"
+                aria-valuemax="100"></div>
+        </div></br>
+        <h4 class="float-left">4 </h4>
+        <div class="progress">
+            <div class="progress-bar" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0"
+                aria-valuemax="100"></div>
+        </div></br>
+        <h4 class="float-left">3 </h4>
+        <div class="progress">
+            <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0"
+                aria-valuemax="100"></div>
+        </div></br>
+        <h4 class="float-left">2 </h4>
+        <div class="progress">
+            <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0"
+                aria-valuemax="100"></div>
+        </div></br>
+        <h4 class="float-left">1 </h4>
+        <div class="progress">
+            <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
+        <div class="text-center font-italic"><p>Rating based on 45 user reviews</p></div>
+        </div> <!-- rating widget end -->
 
+    </br>
+
+        <div class="opentimes-widget text-center">
+            <h3 class="text-center font-weight-bold">Opening times</h3>
+        <table class="table borderless">
+            <tbody>
+                <tr>
+                    <th scope="row">Monday</th>
+                    <td>10:00 - 18:00</td>
+                </tr>
+                <tr>
+                    <th scope="row">Tuesday</th>
+                    <td>10:00 - 18:00</td>
+                </tr>
+                <tr>
+                    <th scope="row">Wednesday</th>
+                    <td>Closed</td>
+                </tr>
+            </tbody>
+        </table>
+        <div class="text-center font-italic"><p>Bank holiday opening hours may vary.</p></div>
+        </div> <!-- opentimes widget end -->
+        
+    </br>
+        
+        <div class="telno-widget text-center">
+            <h3 class="text-center font-weight-bold">Get in touch</h3>
+        <a href="{{'tel:01604123456'}}"><button type="button" class="btn btn-lg btn-primary callbtn"
+                style="margin-right:10px; margin-bottom:10px;"><h4 class="text-white">
+                    <i class="fas fa-phone-alt text-white"></i> 01604 123456</h4></button></a>
+        </div> <!-- telno widget end -->
+
+    </div> <!-- end of col-sm-4 -->
+</div>
+</div>
 
 @endsection
