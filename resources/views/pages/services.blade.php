d52d6181839508a39f5a27a56da29b3e282c781a
rolandudv <u.roland58@gmail.com>
2020-02-27 19:36:36 +0000

    Added Post a review / aligned pagination to center

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -6,6 +6,23 @@
 </div>
 {{-- @for ($i = 0; $i<100; $i++) --}}
 <style>
+    .page-item.active .page-link {
+    z-index: 1;
+    color: #fff;
+    background-color: #FF5864;
+    border-color: #FF5864;
+}
+
+    /* Align pagination in the center */
+    .pagination {
+        display: flex;
+        justify-content: center;
+    }
+
+    .page-item .page-link {
+        font-size: 20px;
+    }
+
     .fa-star {
         color: #FCB700;
     }
@@ -218,6 +235,34 @@
         @endforeach
 
         {{$reviews->links()}}
+
+        <hr class="featurette-divider">
+        @if (\Auth::user() ) <!-- || (\Auth::user()->isStaff == '1') -->
+        <div class="card bg-light font-weight-bold"> <!-- border-dark -->
+            <h3 class="card-header text-white">Post a review</h3>
+            <div class="card-body">
+                <div class="card-text">
+                    <div class="form-group">
+                        <p class="text-white">Fields marked with a * are mandatory</p>
+                        {{Form::label('rating', 'Rate the service * ')}}
+                        {{Form::select('rating', array('5' => '5 ⭐️⭐️⭐️⭐️⭐️', '4' => '4 ⭐️⭐️⭐️⭐️', '3' => '3 ⭐️⭐️⭐️', '2' => '2 ⭐️⭐️', '1' => '1 ⭐️'))}}
+                    </div>
+                    <div class="form-group">
+                        {{Form::label('description', 'Explain why * ')}}
+                        {!! Form::textarea('description', null, ['class'=>'form-control', 'placeholder' => 'Say a few things about your experience']) !!}
+                    </div>
+                    <div class="form-group">
+                        {{Form::submit('Post Review', ['class' => 'btn btn-dark float-right text-white', 'style' => 'margin-top:10px;'])}}
+                        {!! Form::close() !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+        @else
+        <div>
+            <p class="text-center font-italic">You must be signed in to post a review</p>
+        </div>
+        @endif
 
     </div> <!-- don't delete -->
     {{-- @endforeach --}}
