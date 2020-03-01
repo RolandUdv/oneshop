c05cc1ec06d9f1797cf8b0e6130d76bfc64c3a98
rolandudv <u.roland58@gmail.com>
2020-03-01 22:19:43 +0000

    Added calendar to services.show page

---
+++ resources/views/inc/test.blade.php
@@ -0,0 +1,36 @@
+<script type="text/javascript" src="/path/to/jquery.js"></script>
+<script type="text/javascript" src="/path/to/moment.js"></script>
+<script type="text/javascript" src="/path/to/bootstrap/js/transition.js"></script>
+<script type="text/javascript" src="/path/to/bootstrap/js/collapse.js"></script>
+<script type="text/javascript" src="/path/to/bootstrap/dist/bootstrap.min.js"></script>
+<script type="text/javascript" src="/path/to/bootstrap-datetimepicker.min.js"></script>
+
+
+<script>
+
+</script>
+
+
+
+<style>
+
+</style>
+
+
+<div style="overflow:hidden;">
+    <div class="form-group">
+        <div class="row">
+            <div class="col-md-8">
+                <div id="datetimepicker12"></div>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript">
+        $(function () {
+            $('#datetimepicker').datetimepicker({
+                inline: true,
+                sideBySide: true
+            });
+        });
+    </script>
+</div>
--- resources/views/services/show.blade.php
+++ resources/views/services/show.blade.php
@@ -4,25 +4,145 @@
 <div class="container">
     <h1>{{$title}}</h1>
     <br>
+
+    {{-- Datepicker --}}
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+    {{-- Timepicker --}}
+    
+
+    <script>
+        $(function () {
+            $("#datepicker").datepicker({
+                // showOtherMonths: true,
+                // selectOtherMonths: true,
+                // numberOfMonths: 2,
+                firstDay: 1,
+                altField: "#alternate",
+                altFormat: "yy-mm-dd",
+                minDate: 0,
+                maxDate: "+2W",
+            });
+        });
+
+        
+    </script>
+
+    <style>
+        .ui-state-highlight{
+            border: 3px solid #d41a28!important;
+            background: #ffffff!important;
+            /* border-radius: 10px!important; */
+        }
+
+        .ui-datepicker-next,
+        .ui-datepicker-prev {
+            display: none;
+        }
+
+        .ui-datepicker th {
+            font-size: 20px!important;
+            
+        }
+
+        .ui-datepicker-month,
+        .ui-datepicker-year {
+            font-size: 20px!important;
+        }
+
+        .ui-state-default {
+            font-size: 18px!important;
+            margin-bottom: 10px!important;
+            text-align: center!important;
+            /* border-radius: 20px!important; */
+        }
+
+        .ui-state-active {
+            border: 1px solid #d41a28!important;
+            background: #d41a28!important;
+            color: #ffffff!important;
+            font-size: 20px!important;
+            font-weight: 600!important;
+        }
+
+        /* .ui-datepicket-title {
+            background-color: #d41a28!important;
+            color: #ffffff;
+        } */
+
+        .ui-datepicker-inline,
+        .ui-datepicker,
+        .ui-widget,
+        .ui-widget-content,
+        .ui-helper-clearfix,
+        .ui-corner-all,
+        .ui-datepicker-multi-2,
+        .ui-datepicker-multi {
+            width: 100% !important;
+        }
+
+        .submitbtn {
+            background-color: #FF5864 !important;
+            border-color: #ff5764;
+            font-size: 24px;
+        }
+
+        .submitbtn:hover {
+            background-color: #d41a28 !important;
+            border-color: #d41a28;
+        }
+
+        .col-sm-8 {
+            margin-right: -5px;
+        }
+
+        .col-sm-7,
+        .col-sm-4 {
+            background-color: #eaeaea;
+            padding: 20px;
+            border-radius: 20px;
+        }
+    </style>
+
     <div class="row">
-        <div class="col-sm-8">
-            <h4>@include('inc.todaydate')</h4>
-            Put timeslot here
+        <div class="col-sm-7 text-center">
+            <br>
+            {{-- <h4>@include('inc.todaydate')</h4> --}}
+            <br>
+            <div id="datepicker" class="text-center"></div>
+            <br>
+            
+
+
+
+            
         </div> <!-- col-sm-8 end -->
+        <div class="col-sm-1">
+        </div>
         <div class="col-sm-4">
             {!! Form::open(['action' => ['PagesController@create', $service_id], 'method' => 'POST']) !!}
 
             <div class="form-group"></br>
-                <h4 class="font-weight-bold">Service:</h4>
+                <h3>Service:</h3>
                 <input class="form-control" type="text" placeholder="{{$service_name}}" readonly>
             </div>
             <div class="form-group"></br>
-                <h4 class="font-weight-bold">Service Cost:</h4>
+                <h3>Service Cost:</h3>
                 <input class="form-control" type="text" placeholder="£ {{$service_price}}" readonly>
+            </div>
+            <div class="form-group"></br>
+                <h3>Selected Date:</h3>
+            <input id="alternate" type="text" class="form-control" readonly /> 
+            <!-- d-none hides field on all screens -->
+            <!-- d-block d-sm-none displays fields on small displays only -->
             </div>
             <div class="text-center">
                 {{Form::hidden('_method','PUT')}}
-                {{Form::submit('Book Appointment', ['class' => 'btn btn-primary btn-lg btn-block'])}}
+                {{Form::submit('Book Appointment', ['class' => 'submitbtn btn btn-primary btn-lg btn-block', 'style' => 'border-radius: 20px;'])}}
+                {{Form::submit('Pay In Advance', ['class' => 'btn btn-success btn-md btn-block disabled', 'style' => 'border-radius: 20px;'])}}
                 {!! Form::close() !!}
             </div>
         </div> <!-- col-sm-4 end -->
@@ -30,6 +150,16 @@
 
 
 </div> <!-- div container end -->
+
+
+
+
+@include('inc.test')
+
+
+
+
+
 
 
 @endsection
