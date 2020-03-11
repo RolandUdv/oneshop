e499fac8e2d4347e2080f1c0dc5abc228d94759c
rolandudv <u.roland58@gmail.com>
2020-03-11 20:00:29 +0000

    clean up some code

--- routes/web.php
+++ routes/web.php
@@ -30,18 +30,6 @@ Route::post('/services', 'PagesController@storereview');
 // Route::post('/services{$id}', 'PagesController@createbooking'); // may not work !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 Route::get('/services/{service_id}/{service_name}/{service_price}', 'PagesController@show');
 Route::post('/services}', 'PagesController@storebooking'); 
-// Route::put('/services/{service_id}/{service_name}/{service_price}', 'PagesController@storebooking'); 
-
-// Route::post('/services/{service_id}/{service_name}/{service_price}', 'PagesController@storebooking'); 
-// Route::get('/services/{service_id}/{service_name}/{service_price}', 'PagesController@show');
-// {
-//     return $service_id;
-//     return view('favorites');
-// }); 
-
-// may not work !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// Route::input('service_id');
-
 
 // Route::get('/services', 'ReviewController@services');
 Route::get('/scan', 'PagesController@scan');
@@ -50,13 +38,6 @@ Route::get('/profile', 'PagesController@profile');
 Route::get('/store', 'PagesController@store');
 Route::get('/support', 'PagesController@support');
 
-// Route::get('/staff', 'Staff\Controllers\StaffController@staff');
-
-// admin
-// Route::get('/bookings', 'PagesController@bookings');
-// Route::get('/bookings', 'PagesController@bookings');
-// Route::get('/services/create', 'BookingController@create');
-// Route::get('/services/storebooking', 'PagesController@storebooking');
 
 Route::get('/categories', 'CategoryController@categories');
 Route::get('/admin/staff', 'PagesController@staff');
@@ -86,21 +67,6 @@ Route::resource('staff', 'StaffController');
 Route::resource('opentime', 'OpentimesController');
 Route::resource('homepage', 'HomepageController');
 
-// Route::resource('services', 'PagesController', ['except' => ['create']]);
-// Route::resource('index', 'HomepageController');
-
-// Route::resource('service', 'ServiceController');
-// Route::resource('stores', 'StoresController');
-
-// Route::get('/services{id}', function ($id, $username) {
-//     return $id;
-//     return view('services');
-// });
-
-
-
-
-
 
 // dynamic routes
 // profile id pages
@@ -114,27 +80,7 @@ Route::resource('homepage', 'HomepageController');
 //     return view('favorites');
 // });
 
-// store id search
-// Route::get('/store{id}', function ($id, $store_name) {
-//     return $id;
-//     return view('profile');
-// });
 
-/*
-
-// dynamic routes
-// profile id pages
-Route::get('/profile{id}', function ($id, $username) {
-    //return $id;
-    //return view('profile');
-});
-
-// store id search
-Route::get('/store{id}', function ($id, $store_name) {
-    //return $id;
-    //return view('profile');
-});
-*/
 Auth::routes();
 
 Route::get('/', 'HomeController@index');
